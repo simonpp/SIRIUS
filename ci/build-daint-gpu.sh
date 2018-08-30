4cdc9690b34f1b6f519ca8c9f8c03624fe2d48ae
Simon Pintarelli <simon.pintarelli@gmail.com>
2018-08-30 10:42:34 +0200

    jenkins: run build job in cscsci partition

--- ci/build-daint-gpu.sh
+++ ci/build-daint-gpu.sh
@@ -2,6 +2,7 @@
 #SBATCH --job-name=build-daint-gpu
 #SBATCH --nodes=1
 #SBATCH --constraint=mc
+#SBATCH --partition=cscsci
 #SBATCH --time=00:15:00
 #SBATCH --output=build-daint-gpu.out
 #SBATCH --error=build-daint-gpu.err
